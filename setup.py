f84ad0e8021aa5ebb742831bcc5be8455929294b
romanurban <roman.urban.orban@gmail.com>
2025-05-29 00:02:22 +0300

    small tweak

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name="stone_ocr",
     version="0.1.0",
     description="Stone OCR - ML project for stone inscription recognition",
-    author="Your Name",
+    author="urbanroman",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    install_requires=[],  # Use requirements.txt or pyproject.toml for dependencies
+    install_requires=[], 
 )
